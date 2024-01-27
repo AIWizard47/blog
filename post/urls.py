b1094d7ddc5d19c9d19695b927a72cf09122f0d7
Sam <138595608+AIWizard47@users.noreply.github.com>
2024-01-27 17:54:50 +0530

    Code commit

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.index,name='home'),
     path('posts/<str:pk>',views.posts,name='posts'),
+    path('write/submit/URL/blogPostWrite',views.write,name='write'),
+    path('write/submit/URL/post',views.post,name='post'),
     
 ]
+
--- post/views.py
+++ post/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponseNotFound
 from .models import Posts
 
 # Create your views here.
+def custom_404(request, exception):
+
+    # Redirect to the specified URL
+    return redirect(index)
 def index(request):
     posts = Posts.objects.all()
     return render(request,'index.html',{'posts':posts})
@@ -9,3 +14,17 @@ def index(request):
 def posts(request,pk):
     post = Posts.objects.get(id=pk)
     return render(request,'posts.html',{'post':post})
+
+def write(request):
+    return render(request,'write.html')
+
+def post(request):
+    if request.method=='POST':
+        title = str(request.POST['title'])
+        body = str(request.POST['body'])
+        
+        if title.strip() and body.strip():
+            user = Posts.objects.create(title= title,body = body)
+            user.save()
+    # return render(request,'download.html',{'amount':word})
+    return redirect('write')
